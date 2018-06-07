6c5660b9486145bf1f0dbbac21066bf5f12b64a7
gixslayer <ciske_harsema_2@hotmail.com>
2018-06-07 17:45:03 +0200

    Refactored model adapters and bitmap resource fetching api

---
+++ app/src/main/java/rnd/travelapp/adapters/ModelAdapter.java
@@ -0,0 +1,71 @@
+package rnd.travelapp.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class ModelAdapter<T> extends BaseAdapter {
+    private final List<T> models;
+    private final Map<T, String> modelPaths;
+    protected final Context context;
+    private final int layout;
+
+    public ModelAdapter(Map<String, T> models, Context context, int layout) {
+        this.models = new ArrayList<>(models.values());
+        this.modelPaths = new HashMap<>();
+        this.context = context;
+        this.layout = layout;
+
+        models.keySet().forEach(path -> modelPaths.put(models.get(path), path));
+    }
+
+    @Override
+    public int getCount() {
+        return models.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return getModel(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    public T getModel(int i) {
+        return models.get(i);
+    }
+
+    public String getModelPath(int i) {
+        return modelPaths.get(getModel(i));
+    }
+
+    public List<T> getModels() {
+        return models;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup parent) {
+        T model = getModel(i);
+        View view = convertView;
+
+        if(convertView == null) {
+            view = LayoutInflater.from(context).inflate(layout, parent, false);
+        }
+
+        populateView(view, model);
+
+        return view;
+    }
+
+    protected abstract void populateView(View view, T model);
+}
--- app/src/main/java/rnd/travelapp/adapters/ReisModelAdapter.java
+++ app/src/main/java/rnd/travelapp/adapters/ReisModelAdapter.java
@@ -1,93 +1,28 @@
 package rnd.travelapp.adapters;
 
 import android.content.Context;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import rnd.travelapp.R;
 import rnd.travelapp.models.ReisModel;
 
-public class ReisModelAdapter extends BaseAdapter {
-    private final List<ReisModel> reizen;
-    private final Map<ReisModel, String> reisNames;
-    private final Context context;
-
+public class ReisModelAdapter extends ModelAdapter<ReisModel>{
     public ReisModelAdapter(Map<String, ReisModel> models, Context context) {
-        this.reizen = new ArrayList<>(models.values());
-        this.reisNames = new HashMap<>();
-        this.context = context;
-
-        for(String name : models.keySet()) {
-            reisNames.put(models.get(name), name);
-        }
-    }
-    
-    @Override
-    public int getCount() {
-        return reizen.size();
+        super(models, context, R.layout.list_item_reis);
     }
 
     @Override
-    public Object getItem(int i) {
-        return getReis(i);
-    }
-
-    @Override
-    public long getItemId(int i) {
-        return i;
-    }
-
-    /**
-     * Get the reis at the given position.
-     * @param i the position
-     * @return the puzzle at that position
-     */
-    public ReisModel getReis(int i) {
-        return reizen.get(i);
-    }
-
-    public String getModelName(int i) {
-        return reisNames.get(reizen.get(i));
-    }
-
-    /**
-     * Returns the list of reizen in this adapter.
-     * @return the list of reizen
-     */
-    public List<ReisModel> getReizen() {
-        return reizen;
-    }
-
-    @Override
-    public View getView(int i, View convertView, ViewGroup parent) {
-        ReisModel reis = reizen.get(i);
-        View view = convertView;
-
-        if(convertView == null) {
-            view = LayoutInflater.from(context).inflate(R.layout.list_item_reis, parent, false);
-        }
-
+    protected void populateView(View view, ReisModel model) {
         TextView titleView = view.findViewById(R.id.title);
         TextView shortDescriptionView = view.findViewById(R.id.short_description);
         ImageView thumbnailView = view.findViewById(R.id.thumbnail);
 
-        titleView.setText(reis.getReisTitel());
-        shortDescriptionView.setText(reis.getReisKorteBeschrijving());
-
-        reis.getReisAfbeelding().getOrFetch(context, result -> result.consume(thumbnailView::setImageBitmap, cause -> {
-            Log.e("TRAVEL_APP", "Could not fetch image", cause);
-        }));
-
-        return view;
+        titleView.setText(model.getReisTitel());
+        shortDescriptionView.setText(model.getReisKorteBeschrijving());
+        model.getReisAfbeelding().getOrFetchToImageView(context, thumbnailView);
     }
 }
