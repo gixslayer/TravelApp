095dd54f1ddca37ce353057648b65b4ee730f465
gixslayer <ciske_harsema_2@hotmail.com>
2018-06-08 23:38:10 +0200

    Added simple filter adapter

---
+++ app/src/main/java/rnd/travelapp/adapters/TagFilter.java
@@ -0,0 +1,36 @@
+package rnd.travelapp.adapters;
+
+import java.util.List;
+import java.util.function.Function;
+
+import rnd.travelapp.models.ReisModel;
+
+public class TagFilter<T> implements Filter<T> {
+    // Just an example, should be passed inline and removed from here.
+    public static final Function<ReisModel, List<String>> REIS_MODEL_TAGS_EXTRACTOR = ReisModel::getTags;
+
+    private final Function<T, List<String>> tagsExtractor;
+    private String searchString;
+
+    public TagFilter(Function<T, List<String>> tagsExtractor) {
+        this.tagsExtractor = tagsExtractor;
+        this.searchString = "";
+    }
+
+    public void setSearchString(String value) {
+        searchString = value.toLowerCase();
+    }
+
+    public String getSearchString() {
+        return searchString;
+    }
+
+    @Override
+    public boolean filter(T instance) {
+        return tagsExtractor.apply(instance).stream().anyMatch(this::tagMatches);
+    }
+
+    private boolean tagMatches(String tag) {
+        return tag.toLowerCase().contains(this.searchString);
+    }
+}
