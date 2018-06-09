ea31a9f1da1c199b9b89e545fdfa05a3fca7ba07
gixslayer <ciske_harsema_2@hotmail.com>
2018-06-09 14:12:09 +0200

    Added book example

---
+++ app/src/main/java/rnd/travelapp/Booking.java
@@ -0,0 +1,61 @@
+package rnd.travelapp;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import rnd.travelapp.threading.Task;
+import rnd.travelapp.utils.Failable;
+import rnd.travelapp.utils.HttpUtils;
+import rnd.travelapp.utils.StreamUtils;
+
+public class Booking {
+    private final URL bookURL;
+    private final String exampleParam;
+
+    public Booking(Context context, String exampleParam) {
+        this.bookURL = createURL(context);
+        this.exampleParam = exampleParam;
+    }
+
+    public Task<Failable<Integer>> send() {
+        return Task.create(() -> HttpUtils.doPost(bookURL, this::postData, this::parseResponse));
+    }
+
+    private void postData(OutputStream stream) throws IOException, JSONException {
+        JSONObject object = new JSONObject();
+
+        object.put("exampleParam", exampleParam);
+
+        String json = object.toString();
+        byte[] data = json.getBytes(StandardCharsets.UTF_8);
+
+        stream.write(data);
+    }
+
+    private int parseResponse(HttpURLConnection connection, InputStream stream) throws IOException {
+        return Integer.parseInt(StreamUtils.toString(stream));
+    }
+
+    private static URL createURL(Context context) {
+        Resources resources = context.getResources();
+        String serverHost = resources.getString(R.string.server_host);
+        int serverPort = resources.getInteger(R.integer.server_port);
+
+        try {
+            return new URL("http", serverHost, serverPort, "book");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Could not create booking url");
+        }
+    }
+}
--- app/src/main/java/rnd/travelapp/activities/ReisAanbodActivity.java
+++ app/src/main/java/rnd/travelapp/activities/ReisAanbodActivity.java
@@ -1,14 +1,18 @@
 package rnd.travelapp.activities;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.Map;
 
+import rnd.travelapp.Booking;
 import rnd.travelapp.R;
 import rnd.travelapp.adapters.ModelAdapter;
 import rnd.travelapp.adapters.ReisModelAdapter;
 import rnd.travelapp.models.ReisModel;
+import rnd.travelapp.utils.Failable;
 
 public class ReisAanbodActivity extends ModelAdapterActivity<ReisModel> {
     private ListView listView;
@@ -21,6 +25,15 @@ public class ReisAanbodActivity extends ModelAdapterActivity<ReisModel> {
         // Set listeners
         listView = findViewById(R.id.list_reis_aanbod);
         listView.setOnItemClickListener((adapterView, view, i, l) -> openModel(i, ReisModelActivity.class));
+
+        new Booking(this, "test").send().onCompletion(this::onBookingCompleted);
+    }
+
+    private void onBookingCompleted(Failable<Integer> result) {
+        result.consume(
+                bookingID -> Toast.makeText(this, Integer.toString(bookingID), Toast.LENGTH_SHORT).show(),
+                cause -> Log.e("TRAVEL_APP", "Booking failed", cause)
+        );
     }
 
     @Override
