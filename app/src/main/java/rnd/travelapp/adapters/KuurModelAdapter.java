472a32e2fa12048b84fed8ab85bb74427e8ca974
Vincent van 't Hof <v.vanthof@student.science.ru.nl>
2018-06-11 10:37:38 +0200

    KuurModelAdapter WIP

--- app/src/main/java/rnd/travelapp/adapters/KuurModelAdapter.java
+++ app/src/main/java/rnd/travelapp/adapters/KuurModelAdapter.java
@@ -1,4 +1,20 @@
 package rnd.travelapp.adapters;
 
-public class KuurModelAdapter {
+import android.content.Context;
+import android.view.View;
+
+import java.util.Map;
+
+import rnd.travelapp.R;
+import rnd.travelapp.models.KuurModel;
+
+public class KuurModelAdapter extends ModelAdapter<KuurModel> {
+    public KuurModelAdapter(Map<String, KuurModel> models, Context context) {
+        super(models, context, R.layout.list_item_reis);
+    }
+
+    @Override
+    protected void populateView(View view, KuurModel model) {
+
+    }
 }
