42b7eb4fce825556b5f5d7d9d3a9bb86ba799fb1
gixslayer <ciske_harsema_2@hotmail.com>
2018-06-11 20:41:33 +0200

    Added more JavaDoc + small refactoring

--- app/src/main/java/rnd/travelapp/serialization/JSONLoadable.java
+++ app/src/main/java/rnd/travelapp/serialization/JSONLoadable.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Defines the JSONLoader to use while loading the decorated class.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface JSONLoadable {
--- app/src/main/java/rnd/travelapp/serialization/JSONLoaders.java
+++ app/src/main/java/rnd/travelapp/serialization/JSONLoaders.java
@@ -7,29 +7,54 @@ import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Wrapper class to create easy cached access to JSON loaders for a given type.
+ */
 public class JSONLoaders {
     private static final Map<Class, JSONLoader> LOADERS = new HashMap<>();
 
+    /**
+     * Deserialize an instance of the given type from the given JSON string.
+     * @param json the JSON object string to load from
+     * @param type the type to load
+     * @param <T> the type to load
+     * @return the loaded type
+     * @throws JSONException if the loading failed
+     */
     public static <T> T deserialize(String json, Class<T> type) throws JSONException {
-        JSONLoader<T> loader = get(type);
-        JSONObject object = new JSONObject(json);
-
-        return loader.deserialize(object);
+        return get(type).deserialize(new JSONObject(json));
     }
 
+    /**
+     * Returns the JSON loader for the given instance.
+     * @param instance the instance
+     * @param <T> the instance type
+     * @return the loader instance
+     */
     public static <T> JSONLoader<T> get(T instance) {
-        return get(instance.getClass());
-    }
-
-    public static <T> JSONLoader<T> get(Class<?> type) {
-        if(!LOADERS.containsKey(type)) {
-            LOADERS.put(type, create(type));
-        }
-
         //noinspection unchecked
-        return LOADERS.get(type);
+        return get((Class<T>)instance.getClass());
     }
 
+    /**
+     * Returns the JSON loader for the given type.
+     * @param type the type
+     * @param <T> the type
+     * @return the loader instance
+     */
+    public static <T> JSONLoader<T> get(Class<T> type) {
+        synchronized (LOADERS) {
+            //noinspection unchecked
+            return LOADERS.computeIfAbsent(type, JSONLoaders::create);
+        }
+    }
+
+    /**
+     * Creates a new JSON loader for the given type.
+     * @param type the type
+     * @param <T> the type
+     * @return the loader instance
+     */
     private static <T> JSONLoader<T> create(Class<T> type) {
         if(!type.isAnnotationPresent(JSONLoadable.class)) {
             throw new IllegalArgumentException(String.format(
