df11bb0c56d57bff4b06e1da438926e173205fa5
Ciske <ciske_harsema_2@hotmail.com>
2018-06-11 11:24:32 +0200

    Added main activity

--- app/src/main/java/rnd/travelapp/activities/MainActivity.java
+++ app/src/main/java/rnd/travelapp/activities/MainActivity.java
@@ -2,38 +2,30 @@ package rnd.travelapp.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ListView;
+import android.view.View;
 
 import rnd.travelapp.R;
-import rnd.travelapp.adapters.TestModelAdapter;
-import rnd.travelapp.models.TestModel;
 
 public class MainActivity extends VerifyCacheActivity {
-    private ListView listView;
-    private TestModelAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        listView = findViewById(R.id.list_main);
-        listView.setOnItemClickListener((adapterView, view, i, l) -> {
-            Intent intent = new Intent(this, TestModelActivity.class);
-            intent.putExtra("model", adapter.getModelPath(i));
-
-            startActivity(intent);
-        });
     }
 
     @Override
     protected void onCacheVerified() {
-        appCache.getOrFetchList("models/testmodel/list.json", TestModel.class)
-                .onSuccess(models -> {
-                    adapter = new TestModelAdapter(models, this);
-                    listView.setAdapter(adapter);
-                    listView.invalidateViews();
-                }).orOnFailure(cause -> Log.e("TRAVEL_APP", "Failed to fetch list", cause));
+        View aanbodReizen = findViewById(R.id.aanbod_reizen);
+        View aanbodKuren = findViewById(R.id.aanbod_kuren);
+
+        aanbodReizen.setOnClickListener(view -> openActivity(ReisAanbodActivity.class));
+        aanbodKuren.setOnClickListener(view -> openActivity(ReisAanbodActivity.class)); // TODO: Fix to kuur activity
+    }
+
+    private void openActivity(Class<?> activity) {
+        Intent intent = new Intent(this, activity);
+
+        startActivity(intent);
     }
 }
